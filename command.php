b09309136750c3afe847ce3968a137b92a45aaf9
Ben Martinez-Bateman <ben@alwaysblank.org>
2017-12-12 11:35:05 -0800

    Added `clear` method to remove cached blades.

--- command.php
+++ command.php
@@ -3,6 +3,7 @@
 use Roots\Sage\Container;
 use Symfony\Component\Finder\Finder;
 use Illuminate\Contracts\Container\Container as ContainerContract;
+use Illuminate\Filesystem\Filesystem;
 
 if (! class_exists('WP_CLI')) {
     return;
@@ -112,6 +113,57 @@ class BladeCommand extends WP_CLI_Command
         if (!$sush) :
             WP_CLI\Utils\format_items('table', $list, array( 'File', 'Status' ));
         endif;
+    }
+
+    /**
+     * Clears cached files for your Blades.
+     *
+     * NOTE: This removes the cached files _for the Blades it finds_
+     * it does _not_ remove all cached files. For that use, `wp blade wipe`.
+     *
+     * ## OPTIONS
+     *
+     * [--directory=<directory>]
+     * : Specify template directory below theme root
+     * (defaults to theme root)
+     * ---
+     * default: all
+     * ---
+     *
+     * ## EXAMPLES
+     *
+     *     # Only clear layouts
+     *     wp blade clear --directory=views/layouts
+     *
+     *     # Clear all templates
+     *     wp blade clear
+     *
+     * @when after_wp_load
+     *
+     */
+    public function clear($args, $assoc_args)
+    {
+        $sush = WP_CLI::get_config('quiet');
+        $finder = new Finder();
+        $blade = App\sage('blade');
+        $filesystem = new Filesystem();
+
+        $directory = $this->determineDirectory(isset($assoc_args['directory']) ? $assoc_args['directory'] : false);
+
+        $finder->files()->name('/^.+\.blade\.php$/i')->in($directory);
+
+        $cache_files = [];
+        foreach ($finder as $file) {
+            $cache_files[] = $blade->compiledPath($file->getRealPath());
+        }
+
+        $success = $filesystem->delete($cache_files);
+
+        if ($success) {
+            WP_CLI::success("Templates cleared!");
+        } else {
+            WP_CLI::error("Uh-oh, something went wrong!");
+        }
     }
 }
 WP_CLI::add_command('blade', 'BladeCommand');
